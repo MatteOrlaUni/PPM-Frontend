c5df5decef347b221ef0b6710f7e92c479ae460e
TestinaSan <matteoorlandi27@gmail.com>
2026-04-28 21:43:32 +0200

    Risolti problemi per telefono. Header responsivo completato

--- js/index.js
+++ js/index.js
@@ -18,19 +18,29 @@ const dataISO = oggi.toISOString().split('T')[0];
 timeElement.setAttribute('datetime', dataISO);
 
 
-// aggiunge logo appena inizio a scorrere la pagina
-window.addEventListener("scroll", function() {
-    const img = document.getElementById('logo-laterale');
+// gestione sezione centrale header
+window.addEventListener('scroll', function() {
     const menu = document.getElementById('menu-header-scomparsa');
     const hr = document.getElementById('menu-header-scomparsa-hr');
+    const img = document.getElementById('logo-laterale');
 
     if (window.scrollY > 0) {
-        img.classList.add('apparizione-logo');
         menu.classList.add('apparizione-testata');
         hr.classList.add('apparizione-testata');
+
+        if (window.innerWidth > 600)
+            img.classList.add('apparizione-logo');
     } else {
-        img.classList.remove('apparizione-logo');
         menu.classList.remove('apparizione-testata');
         hr.classList.remove('apparizione-testata');
+        img.classList.remove('apparizione-logo');
     }
+})
+
+// toglie il logo quando lo schermo è piccolo
+window.addEventListener('resize', function() {
+    const img = document.getElementById('logo-laterale');
+
+    if (window.innerWidth < 600)
+        img.classList.remove('apparizione-logo');
 })
